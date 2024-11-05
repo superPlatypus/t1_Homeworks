8db60365813c967963346e7a16accf04bb4a2448
Kirill Pashovkin <kirillpashovkin@gmail.com>
2024-11-05 14:44:28 +0300

    Домашнее задание 3 - Spring Boot Starter.

---
+++ simpleService/src/test/java/ru/platypus/simpleservice/SimpleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.platypus.simpleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
